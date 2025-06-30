1f981fc12d458977972fca6656b97de558e990fc
silofn523 <silofn523@gmail.com>
2025-06-30 16:04:32 +0900

    test임 ㅇㅇ

--- src/answer/dto/answer/create-answer.dto.ts
+++ src/answer/dto/answer/create-answer.dto.ts
@@ -6,7 +6,7 @@ export class CreateAnswerDto {
   @IsString()
   @ApiProperty({
     description: '사용자가 입력한 텍스트',
-    default: '잘 모르겠습니다.'
+    default: '잘 모르겠습니다..'
   })
   public userResponse!: string
 }
