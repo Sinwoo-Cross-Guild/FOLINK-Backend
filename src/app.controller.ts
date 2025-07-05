ee25e4e4d80a8b2b56bac82f719db6ea37c5d896
silofn523 <silofn523@gmail.com>
2025-07-06 02:29:54 +0900

    d

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,14 +3,15 @@ import { Body, Controller, Get, NotFoundException, Param, ParseIntPipe, Post, Va
 import { AppService } from './app.service'
 import { Test } from './app.test.entity'
 import { TestDto } from './app.test.dto'
+import { ok } from 'assert'
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello()
+  @Get('/health')
+  getHello(): { status: string } {
+    return { status: 'ok' }
   }
 
   @Get('test')
@@ -18,11 +19,11 @@ export class AppController {
     return this.appService.getDate()
   }
 
-  @Get("test/:id") 
+  @Get('test/:id')
   getOneTestData(@Param('id', ParseIntPipe) id: number) {
     const test = this.appService.getOneDate(id)
 
-    if(!test){
+    if (!test) {
       throw new NotFoundException(`${id} Not found.`)
     }
 
